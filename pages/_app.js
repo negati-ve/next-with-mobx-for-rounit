a3274109b54a82c07e5c2139ae9f14549ee7abe8
Negati-ve <zeeshan@Mercury.local>
2022-05-07 00:00:05 +0530

    connect mobx to app via provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,40 @@
 import '../styles/globals.css'
+import { Provider } from "mobx-react";
+import App from "next/app";
+import React from "react";
+import {
+  initializeStore,
+} from "../stores/";
+import Head from "next/head";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export function reportWebVitals(metric) {
+  console.log(metric);
 }
+export default class MyApp extends App {
 
-export default MyApp
+  constructor(props) {
+    super(props);
+    const isServer = typeof window === "undefined";
+    this.mobxStore = initializeStore(props.initialMobxState);
+  }
+
+  componentDidMount() {
+    if (process.env.NODE_ENV == "production")
+      TagManager.initialize(tagManagerArgs);
+  }
+
+  render() {
+    const { Component, pageProps } = this.props;
+    return (
+      <Provider store={this.mobxStore}>
+        <Head>
+          <meta
+            name="viewport"
+            content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"
+          />
+        </Head>
+        <Component {...pageProps} />
+      </Provider>
+    );
+  }
+}
